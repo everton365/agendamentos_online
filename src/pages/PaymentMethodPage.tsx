493e2225c800e2bf15796fbe6ef458234b8c3dfd
everton365 <evertonapk09@gmail.com>
2025-10-07 23:27:30 -0300

    ajuste botao de pagamento

--- src/pages/PaymentMethodPage.tsx
+++ src/pages/PaymentMethodPage.tsx
@@ -337,7 +337,7 @@ const PaymentMethodPage = () => {
               </span>
             </h1>
             <p className="text-xl text-muted-foreground max-w-2xl mx-auto">
-              Para confirmar seu agendamento, click em 'Pagar com Mercado Pago'.
+              Para confirmar seu agendamento, click em ' Finalizar pagamento'.
             </p>
           </div>
 
@@ -514,7 +514,7 @@ const PaymentMethodPage = () => {
                     ) : (
                       <>
                         <CheckCircle className="w-5 h-5 mr-2" />
-                        Pagar com Mercado Pago {formatPrice(adjustedPrice)}
+                        Finalizar pagamento {formatPrice(adjustedPrice)}
                       </>
                     )}
                   </Button>
